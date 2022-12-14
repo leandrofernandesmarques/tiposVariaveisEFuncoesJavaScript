a5ecdf5875b5bbf8ac1aa12ac1bbd0690c480a99
Leandro Marques <leandromarques@iMac-de-Leandro.local>
2022-12-13 22:02:46 -0300

    ultimo

--- expressoes-funcao.js
+++ expressoes-funcao.js
@@ -11,7 +11,7 @@ minhaFuncao("param")
 const soma = function(num1, num2) { return num1 + num2 }
 console.log(soma(1, 1))
 
-// diferença principal: HOISTING
+// diferença principal: HOISTING(içar para cima)
 // funções e var são "listadas" no topo do arquivo.
 
 console.log(apresentar())
@@ -22,3 +22,5 @@ function apresentar() {
 
 console.log(somaOutroExemplo(1, 1))
 const somaOutroExemplo = function(num1, num2) { return num1 + num2 }
+
+//A funçao é içada, mas a variável não.
