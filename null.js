5922c9a635b9156f7a481b0cdca21fe9861a7494
Leandro Marques <leandromarques@iMac-de-Leandro.local>
2022-12-13 22:27:50 -0300

    Estudo de Tipos, Variáveis e funções em JavaScript

--- null.js
+++
@@ -1,9 +0,0 @@
-//Tipo null: Definido como nulo, diferente de undefined: Não definido
-
-let input = null
-
-if (input === null) {
- console.log('não há informação')
-} else {
- console.log(input)
-}
